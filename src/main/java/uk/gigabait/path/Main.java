9770e64d5906eda6de1dc305215d2c5bb34aca72
GIGABAIT93 <59637348+GIGABAIT93@users.noreply.github.com>
2025-04-11 02:36:39 +0300

    Update Main.java

--- src/main/java/uk/gigabait/path/Main.java
+++ src/main/java/uk/gigabait/path/Main.java
@@ -2,11 +2,12 @@ package uk.gigabait.path;
 
 import me.hsgamer.bettergui.api.addon.GetPlugin;
 import me.hsgamer.bettergui.api.addon.Reloadable;
+import me.hsgamer.bettergui.api.addon.PostEnable;
 import me.hsgamer.hscore.expansion.common.Expansion;
 import me.hsgamer.hscore.expansion.extra.expansion.DataFolder;
 import uk.gigabait.path.util.Config;
 
-public final class Main implements Expansion, Reloadable, DataFolder, GetPlugin {
+public final class Main implements Expansion, Reloadable, DataFolder, GetPlugin, PostEnable {
 
     @Override
     public boolean onLoad() {
@@ -30,5 +31,11 @@ public final class Main implements Expansion, Reloadable, DataFolder, GetPlugin 
     @Override
     public void onDisable() {
         Config.clear();
+    }
+
+    @Override
+    public void onPostEnable() {
+        // I do not know why but the right menu processing with a custom path is processed correctly after the plugin is reprinted
+        getPlugin().getServer().dispatchCommand(getPlugin().getServer().getConsoleSender(), "reloadplugin");
     }
 }
